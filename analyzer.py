0951b75e4b94bb81a5dc873d4792159206cb4810
viktoriia-kravchuk <vikakravchuk2@gmail.com>
2020-04-15 15:41:05 +0300

    VK 2020-04-15

--- analyzer.py
+++ analyzer.py
@@ -1,6 +1,8 @@
 #import bibliotek
 import os
 import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
 
 #wyświetlenie zawartości katalogu z opiniami
 print(*os.listdir("./opinions_json"))
@@ -12,6 +14,33 @@ product_id = input("Podaj kod produktu: ")
 opinions = pd.read_json("./opinions_json/"+product_id+'.json')
 opinions=opinions.set_index("opinion_id")
 
+opinions["stars"]=opinions["stars"].map(lambda x: float(x.split("/")[0].replace(",",".")))
 
+#częstość występowania poszczególnej liczby gwiazdek
+stars=opinions["stars"].value_counts().sort_index().reindex(list(np.arange(0,5.1,0.5)),fill_value=0)
+fig, ax=plt.subplots()
+stars.plot.bar(color="deepskyblue")
+plt.xticks(rotation=0)
+ax.set_title("Częstość występowania poszczególnych ocen")
+ax.set_xlabel("liczba gwiazdek")
+ax.set_ylabel("liczba opinii")
+plt.savefig("./figures_png/"+product_id+'_bar.png')
+plt.close()
 
-print(opinions)
+#udział poszczególnych rekomendacji w ogólnej liczbie opinii 
+recommendation=opinions['recommendation'].value_counts()
+fig, ax=plt.subplots()
+recommendation.plot.pie(label="",autopct="%.1f%%",colors=["mediumseagreen","coral"])
+ax.set_title("Udział poszczególnych rekomendacji w ogólnej liczbie opinii ")
+plt.savefig("./figures_png/"+product_id+'_pie.png')
+plt.close()
+
+#podstawowe statystyki
+stars_everage=opinions["stars"].mean()
+pros=opinions["pros"].count()
+cons=opinions["cons"].count()
+purchased=opinions["purchased"].sum()
+print(stars_everage,pros,cons,purchased)
+
+stars_purchased=pd.crosstab(opinions["stars"],opinions["purchased"])
+print(stars_purchased)
